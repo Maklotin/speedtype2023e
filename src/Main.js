a3a9ba5f9ca9ae94a0661f998e9566d29f3e52d3
Maklotin <mikkel.huth@gmail.com>
2023-05-22 18:04:41 +0200

    UÆHHHHHHHHHH

--- src/Main.js
+++ src/Main.js
@@ -1,43 +1,126 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import './App.css';
 import logo from './bilder/logo1-1.png'
 import { db } from './backend/firebase-config'
-import { collecion } from 'firebase/firestore'
+import { collection, getDocs, addDoc } from 'firebase/firestore'
 
 
 function Main() {
     const [aktivSideMain, setAktivSideMain] = useState("SpeedType")
-    const [poeng, setPoeng] = useState('0');
     const [bruker, setBruker] = useState([])
+    const usersCollectionRef = collection(db, "brukere")
+
+    const [passordFelt1, setPassordFelt1] = useState("")
+    const [passordFelt2, setPassordFelt2] = useState("")
+    const [brukerLoggetInn, setBrukerLoggetInn] = useState(false)
+    const [nyBrukernavn, setNyBrukernavn] = useState("")
+    const [nyPassord, setNyPassord] = useState("")
+    const [highscore, setHighscore] = useState(0)
+    const [poeng, setPoeng] = useState(0);
+    const [error, setError] = useState("")
+
+    const lagBruker = async () => {
+        if (passordFelt1 === passordFelt2) {
+            setNyPassord(passordFelt1)
+            await addDoc(usersCollectionRef, { brukernavn: nyBrukernavn, passord: nyPassord })
+        } else {
+            setError("Passordet matcher ikke!")
+        }
+    }
+
+    const handleNyBrukernavnChange = useCallback(
+        (event) => {
+          setNyBrukernavn(event.target.value);
+        },
+        []
+      );
+    
+      const handlePassordFelt1Change = useCallback(
+        (event) => {
+          setPassordFelt1(event.target.value);
+        },
+        []
+      );
+    
+      const handlePassordFelt2Change = useCallback(
+        (event) => {
+          setPassordFelt2(event.target.value);
+        },
+        []
+      );
+
+    if (poeng < highscore) {
+        setHighscore(poeng)
+    } else { 
+        
+    }
 
     useEffect(() => {
         /*https://www.youtube.com/watch?v=jCY6DH8F4oc&t=853s*/
-        const usersCollectionRef = collection(db, "brukere")
-        const getBrukere = async => {
 
+        const getBrukere = async () => {
+            const data = await getDocs(usersCollectionRef)
+            setBruker(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
         }
 
         getBrukere()
-    }, [])
+    }, []);
 
     var avnsitt = "ingen tekst funnet"
 
-    const SpeedType = () => {
+    const LoggetInn = () => {
         return (
             <>
                 <div id="f_spillet">
                     <img src={logo} alt="SpeedType logo" id="logo_speedtype"></img>
                     <hr id="kort_strek"></hr>
                     <div id="start_spill">
-                        <button class="knapper"><p>Start</p></button>
+                        <button class="knapper"><h3>Start</h3></button>
                     </div>
                     <p>{avnsitt}</p>
-                    <input id="ST_input" placeholder="skriv inn teksten her"></input>
+                    <input className="ST_input" placeholder="skriv inn teksten her"></input>
                 </div>
-
+                <div id="highscores">
+                    {bruker.map((bruker) => { return <div>
+                         {" "}
+                         <p>brukernavn: {bruker.brukernavn}</p>
+                         </div> })}
+                </div>
             </>
         )
     }
+
+    const IkkeLoggetInn = () => {
+        return (
+            <>
+                <div id="ikkeloggetinn">
+                    <div id="logginn">
+                        <h1>Logg Inn</h1>
+                        <input className="ST_input" type="text" placeholder="brukernavn..."></input>
+                        <input className="ST_input" type="password" placeholder="passord..."></input>
+                        <button className="knapper"><p>Logg Inn</p></button>
+                    </div>
+                    <div id="lagnybruker">
+                        <h1>Registrer Bruker</h1>
+                        <input value={nyBrukernavn} onChange={handleNyBrukernavnChange} className="ST_input" type="text" placeholder="brukernavn"></input>
+                        <input value={passordFelt1} onChange={handlePassordFelt1Change} className="ST_input" type="password" placeholder="passord"></input>
+                        <input value={passordFelt2} onChange={handlePassordFelt2Change} className="ST_input" type="password" placeholder="bekreft passord"></input>
+                        <p id="error">{error}</p>
+                        <button onClick={lagBruker} className="knapper"><p>Registrer Bruker</p></button>
+                    </div>
+                </div> 
+            </>
+        )
+    }
+
+    const SpeedType = () => {
+        if (brukerLoggetInn === true) {
+            return <LoggetInn />
+        } else {
+            return <IkkeLoggetInn />
+        }
+    }
+
 
     const IntrVideo = () => {
         return (
@@ -90,7 +173,7 @@ function Main() {
                 <button onClick={() => setAktivSideMain("FAQ")} className="knapper"><p>FAQ</p></button>
                 <button onClick={() => setAktivSideMain("LoverOgRegler")} className="knapper"><p>Lover og regler</p></button>
             </div>
-            <hr id="strek"></hr>
+            <hr className="strek"></hr>
 
             <AktivSideMainFunc />
         </>
--- src/backend/firebase-config.js
+++ src/backend/firebase-config.js
@@ -1,19 +1,33 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore'
-import dotenv from 'dotenv'
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+/*
 const firebaseConfig = {
   apiKey: process.env.REACT_KEY_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
   projectId: process.env.REACT_APP_PROJECTID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env_MESSAGESENDERID,
+  messagingSenderId: process.env.REACT_APP_MESSAGESENDERID,
   appId: process.env.REACT_APP_APPID
+};*/
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtYsXg1bc23HNpGNrKAH_GXsrLktjl-M8",
+  authDomain: "speedtype-268c7.firebaseapp.com",
+  projectId: "speedtype-268c7",
+  storageBucket: "speedtype-268c7.appspot.com",
+  messagingSenderId: "137204822275",
+  appId: "1:137204822275:web:79851dd6a3f0b5f6a53936"
 };
+
 
 
 // Initialize Firebase
