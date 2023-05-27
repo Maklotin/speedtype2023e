6a82b2bcaccae5a77ec71cfbe2b07c3006b02a22
Maklotin <mikkel.huth@gmail.com>
2023-05-27 23:16:05 +0200

    jobber med spillet

--- src/frontend/SpeedTypeSpill.js
+++ src/frontend/SpeedTypeSpill.js
@@ -1,58 +1,92 @@
-import logo from '../bilder/logo1-1.png'
-import React, { useState, useEffect } from 'react';
-import { db } from '../backend/firebase-config'
-import { collection, getDocs, addDoc } from 'firebase/firestore'
+import logo from '../bilder/logo1-1.png';
+import React, { useState, useEffect, useRef } from 'react';
+import { db } from '../backend/firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
 import './App.css';
+import tekstUtdragData from './planb.json';
+
+
+
+const sekunder = 30
+
 
 const SpeedTypeSpill = () => {
+    const [valgtTekstUtdrag, setValgtTekstUtdrag] = useState('');
+    const [spTkstFlt, setSpTkstFlt] = useState('');
+    const [score, setScore] = useState(0);
+    const [highscore, setHighscore] = useState(0);
+
+    //https://www.youtube.com/watch?v=t4W7PN4js-8
+    var alleTekstUtdrag = []
+
+    for (let i = 0; i <= 10; i++) {
+        alleTekstUtdrag.push(tekstUtdragData[i])
+        console.log(alleTekstUtdrag)
+    }
 
     useEffect(() => {
-        /*https://www.youtube.com/watch?v=jCY6DH8F4oc&t=853s*/
-
         const getBrukere = async () => {
-            const data = await getDocs(usersCollectionRef)
-            setBruker(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-        }
+            const data = await getDocs(collection(db, 'brukere'));
+            setBruker(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        };
 
-        getBrukere()
+        getBrukere();
     }, []);
 
-    var avnsitt = "ingen tekst funnet"
+    const [bruker, setBruker] = useState([]);
 
-    const [bruker, setBruker] = useState([])
-
-    const usersCollectionRef = collection(db, "brukere")
     return (
         <>
             <div id="f_spillet">
                 <img src={logo} alt="SpeedType logo" id="logo_speedtype"></img>
                 <hr id="kort_strek"></hr>
                 <div id="start_spill">
-                    <button class="knapper"><h3>Start</h3></button>
+                    <button className="knapper">
+                        <h3>Start</h3>
+                    </button>
                 </div>
-                <p>{avnsitt}</p>
-                <input className="ST_input" placeholder="skriv inn teksten her"></input>
+                <div className="innhold">
+                    {tekstUtdragData.map}
+                    {/*tekstUtdrag.map((word, i) => {
+                        <span>
+                            {word}
+                        </span>
+                    })*/}
+                </div>
+                <input
+                    className="ST_input"
+                    placeholder="skriv inn teksten her"
+                ></input>
+                <p>Poeng: {score}</p>
             </div>
             <div id="highscores">
                 <table>
-                    <tr>
-                        <th><h1>Brukernavn</h1></th>
-                        <th><h1>Highscore</h1></th>
-                    </tr>
-                    {bruker.map((bruker) => {
-                        return <div>
-                            {" "}
-                            <tr>
-                                <td><p>{bruker.brukernavn}</p></td>
-                                <td><p>{bruker.highscore}</p></td>
+                    <thead>
+                        <tr>
+                            <th>
+                                <h1>Brukernavn</h1>
+                            </th>
+                            <th>
+                                <h1>Highscore</h1>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {bruker.map((bruker) => (
+                            <tr key={bruker.id}>
+                                <td>
+                                    <p>{bruker.brukernavn}</p>
+                                </td>
+                                <td>
+                                    <p>{bruker.highscore}</p>
+                                </td>
                             </tr>
-                        </div>
-                    })}
+                        ))}
+                    </tbody>
                 </table>
-
             </div>
         </>
-    )
-}
+    );
+};
 
 export default SpeedTypeSpill;
